9cca6507be08f106810c84f123f712c29d3350d9
김태완 <gimtaewan@gimtaewan-ui-MacBook-Pro.local>
2017-04-25 03:03:29 +0900

    user seed만들고 db schema error 수정

--- db/schema.rb
+++ db/schema.rb
@@ -23,13 +23,18 @@ ActiveRecord::Schema.define(version: 20170422161351) do
   end
 
   create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "name",                   default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
