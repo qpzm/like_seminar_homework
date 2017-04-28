f65c8d22732478bf13dba74515354c2676af070d
Hmhm lee <lhm1442@gmail.com>
2017-04-29 00:28:22 +0900

    실습용 ajax 코드 지움

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,6 @@ class CommentsController < ApplicationController
     @comment.post_id=params[:post_id]
     @comment.user_id=current_user.id
     @comment.save
-    render json: {content: @comment.content, user_name: @comment.user.name, updated_at: @comment.updated_at}
-    #redirect_to :back
+    redirect_to :back
   end
 end
